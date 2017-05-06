1d21e1030cb4b725eab733f16f582121fcfa453c
Ornella Hernandez <ohernandez2016@u.northwestern.edu>
2017-05-06 12:22:15 -0500

    final

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "survey.html"));
+});
+
